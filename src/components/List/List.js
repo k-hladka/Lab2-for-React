b7d2d000af856c0d871c9cba9eab2c927d6cf89e
Kateryna Hladka <zipz211_gkv@student.ztu.edu.ua>
2023-12-04 15:00:03 +0200

    Change: card list to product list. Added: virtualization for list

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,36 +1,42 @@
 import React, {useEffect, useState} from "react";
 import "./style.css";
 import {Logo} from "./Logo";
+import {FixedSizeList} from "react-window";
 
-const dataCard = (setList) => {
-    fetch('https://dummyjson.com/carts')
+const data = (setList) => {
+    fetch('https://dummyjson.com/products')
         .then(res => res.json())
         .then(data => setList(data));
 };
 
 export function List() {
-    const [listAllElements, setListAllElements] = useState({});
-    useEffect(() => dataCard(setListAllElements), []);
+    let [list, setList] = useState({});
+    useEffect(() => data(setList), []);
+    const ListLi = ({index, style}) => {
+        const product = list?.products[index];
+        if (!product){
+            return null
+        }
+        return (
+            <li key={product.id} style={style}>
+                <Logo backgroundImage={product.images[0]}/>
+                {product.title}
+                <br></br>
+                Brand - {product.brand}
+                <br></br>
+                Price = {product.price}
+            </li>)
+    }
 
     return (
         <>
-            <h3 className="Listh3">Carts list</h3>
-            <div className="ListDiv">
-                <ul>{listAllElements?.carts?.map((cart) =>
-                    <li key={cart.id}>
-                        <ol>{cart?.products?.map((product) =>
-                            <li key={product.id} className="p">
-                                <Logo title={product.title}/>
-                                {product.title}
-                                <br></br>
-                                Price = {product.price}
-                            </li>)
-                        }
-                        </ol>
-                    </li>)
-                }
-                </ul>
-            </div>
+            <h3 className="Listh3">Products List</h3>
+            <FixedSizeList className="List ListDiv"
+                           height={400}
+                           itemCount={list?.products?.length}
+                           itemSize={70}>
+                {ListLi}
+            </FixedSizeList>
         </>
     );
 }
--- src/components/List/Logo.js
+++ src/components/List/Logo.js
@@ -1,17 +1,7 @@
 export function Logo(props) {
-    const {title} = props;
-    let firstLetter = title.substring(0, 1).toUpperCase();
-
+    const {backgroundImage} = props;
     return (
-        <div className="LogoDiv" style={{backgroundColor: randomColor()}}>
-            <span>{firstLetter}</span>
+        <div className="LogoDiv" style={{backgroundImage: 'url(' + backgroundImage + ')'}}>
         </div>
     );
-}
-
-function randomColor() {
-    let rgb = new Array(3)
-        .fill(1)
-        .map(() => Math.round((Math.random() * 255) + 1));
-    return `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]}, 0.5)`;
 }
